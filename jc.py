295f02c572f974fe86a45a4f17ef1e7379b4efff
AgJonce <waltairmoraes@gmail.com>
2026-06-21 16:10:20 -0300

    Update jc.py

--- jc.py
+++ jc.py
@@ -404,80 +404,80 @@ def receitas():
 
             dados = df[
                 df["selecao"] == receita_selecionada
-            ].iloc[0]
+                ].iloc[0]
 
-            st.session_state["receita_id"] = int(
-            dados["id"]
-            )
-
-            st.session_state["descricao"] = dados["descricao"]
-            st.session_state["categoria"] = dados["categoria"]
-            st.session_state["conta"] = dados["conta"]
-            st.session_state["forma_recebimento"] = dados["forma_recebimento"]
-            st.session_state["valor"] = float(dados["valor"])
-            st.session_state["observacao"] = dados["observacao"]
-
-            st.divider()
-
-            st.subheader("📄 Receita Selecionada")
-
-            col1, col2 = st.columns(2)
-
-            with col1:
-
-                st.text_input(
-                    "Descrição",
-                    value=dados["descricao"],
-                    disabled=True
+                st.session_state["receita_id"] = int(
+                dados["id"]
                 )
+
+                st.session_state["descricao"] = dados["descricao"]
+                st.session_state["categoria"] = dados["categoria"]
+                st.session_state["conta"] = dados["conta"]
+                st.session_state["forma_recebimento"] = dados["forma_recebimento"]
+                st.session_state["valor"] = float(dados["valor"])
+                st.session_state["observacao"] = dados["observacao"]
+
+                st.divider()
+
+                st.subheader("📄 Receita Selecionada")
+
+                col1, col2 = st.columns(2)
+
+                with col1:
+
+                    st.text_input(
+                        "Descrição",
+                        value=dados["descricao"],
+                        disabled=True
+                    )
     
+                    st.text_input(
+                        "Categoria",
+                        value=dados["categoria"],
+                        disabled=True
+                    )
+
+                    st.text_input(
+                        "Conta",
+                        value=str(dados["conta"]),
+                        disabled=True
+                    )
+
+                with col2:
+
+                    st.text_input(
+                        "Data",
+                        value=str(dados["data"]),
+                        disabled=True
+                    )
+
                 st.text_input(
-                    "Categoria",
-                    value=dados["categoria"],
+                    "Forma Recebimento",
+                    value=str(dados["forma_recebimento"]),
                     disabled=True
                 )
 
                 st.text_input(
-                    "Conta",
-                    value=str(dados["conta"]),
+                    "Valor",
+                    value=f"R$ {dados['valor']:,.2f}",
                     disabled=True
                 )
 
-            with col2:
-
-                st.text_input(
-                    "Data",
-                    value=str(dados["data"]),
-                    disabled=True
-                )
-
-            st.text_input(
-                "Forma Recebimento",
-                value=str(dados["forma_recebimento"]),
+            st.text_area(
+                "Observação",
+                value=str(dados["observacao"]),
                 disabled=True
             )
 
-            st.text_input(
-                "Valor",
-                value=f"R$ {dados['valor']:,.2f}",
-                disabled=True
+            st.success(
+                f"Receita ID {dados['id']} carregada para alteração ou exclusão."
             )
 
-        st.text_area(
-            "Observação",
-            value=str(dados["observacao"]),
-            disabled=True
-        )
+        else:
 
-        st.success(
-            f"Receita ID {dados['id']} carregada para alteração ou exclusão."
-        )
-
-    else:
-
-        st.warning(
-            "Nenhuma receita encontrada."
-        )
+            st.warning(
+                "Nenhuma receita encontrada."
+            )
     # ALTERAR
     # ==========================
 
